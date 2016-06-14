24ffec3145ef3e247ee28f1ae80d414869b9c814
Jason Nall <jsonnull@gmail.com>
2016-06-14 17:52:32 -0400

    Upgrade to latest typeset version.

--- tests/index.js
+++ tests/index.js
@@ -4,10 +4,33 @@ var gutil = require('gulp-util');
 var expect = require('chai').expect;
 var typeset = require('../');
 
-it('should ', function (cb) {
+it('should run', function (cb) {
 	var stream = typeset();
 	var html = '<p>"Hello," said the fox.</p>';
 	var result = '<p><span class="pull-double">“</span>Hello,” said the fox.</p>';
+  var file = new gutil.File({
+		base: __dirname,
+		path: __dirname + '/test.html',
+		contents: new Buffer(html)
+	});
+
+	stream.on('data', function (file) {
+		expect(file.contents.toString()).to.equal(result);
+	});
+
+	stream.on('end', cb);
+
+	stream.write(file);
+
+	stream.end();
+});
+
+it('should work with disable option', function (cb) {
+	var stream = typeset({
+		disable: ['ligatures']
+	});
+	var html = '<p>Ligature on first</p>';
+	var result = '<p>Ligature on first</p>';
   var file = new gutil.File({
 		base: __dirname,
 		path: __dirname + '/test.html',
